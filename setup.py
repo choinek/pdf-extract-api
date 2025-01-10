ecb5cf86ff1f1e2b0c1d99fe92ceff9713b16db3
Adrian Chojnicki <adrian@chojnicki.pl>
2025-01-10 03:48:14 +0100

    #63 setup py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='text-extract-api',
+    version='0.1.0',
+    packages=[''],
+    package_dir={'': 'text-extract-api'},
+    url='https://github.com/CatchTheTornado/text-extract-api',
+    license='GPL3',
+    author='',
+    author_email='',
+    description=''
+)
